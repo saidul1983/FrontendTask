8240587bba755223c0e3af6d9146393421b6d0b5
Saidul Islam Rajib <saidul.rajib@brainstation-23.com>
2025-12-17 20:08:56 +0600

    Disabled dev indicators and remove unnecessary files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,4 @@
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
-import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
-import Features from "@/components/Features";
-import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -17,7 +9,6 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <Hero />
       <Blog />
     </>
   );
